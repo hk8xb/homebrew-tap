e9f9e8ca6a132e6b6c8ec07e0779f4e24da5f0c9
Andrew Kwag <andrew.hoshin.kwag@gmail.com>
2026-04-08 13:20:27 -0400

    Update gentabase formula to v0.1.0 with real SHA256 checksums

--- Formula/gentabase.rb
+++ Formula/gentabase.rb
@@ -6,20 +6,20 @@ class Gentabase < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/hk8xb/gentabase-cli/releases/download/v#{version}/gentabase_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "16fe7cf72ce229dd44f9e96d15b1b897926d8b8d094b1c192c3aedd7e18ee7a4"
     else
       url "https://github.com/hk8xb/gentabase-cli/releases/download/v#{version}/gentabase_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_AMD64"
+      sha256 "70be47809df01afef431a75bd58dc0bc542ab4fe0fd0da09e9d76d3d215aa562"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/hk8xb/gentabase-cli/releases/download/v#{version}/gentabase_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "a9f6024bcfc37300f0724c6d5b304658fdc744cc97b825c07ad8e27d199c8a44"
     else
       url "https://github.com/hk8xb/gentabase-cli/releases/download/v#{version}/gentabase_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AMD64"
+      sha256 "93c171052b0d6209c7bb0aa7b84ffcebae8c864fd622725139af22ddfb4a28ff"
     end
   end
 
